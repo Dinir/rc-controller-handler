789a0ef25e35a20cb27ebbfdd8621f9f8ccff3f8
Dinir Nertan <DinirNertan@gmail.com>
2025-09-26 14:46:00 +0900

    add helper plane for propeller animation

--- Assets/TD_DroneControl/DroneMovement/HelperPlane.cs
+++ Assets/TD_DroneControl/DroneMovement/HelperPlane.cs
@@ -1,7 +1,24 @@
-namespace TD_DroneControl.DroneMovement
+using UnityEngine;
+
+namespace DroneMovement
 {
-    public class HelperPlane
+    public class HelperPlane : MonoBehaviour
     {
-        
+        [SerializeField] private float maxTiltDegree = 30f;
+
+        public void Tilt(Vector2 d)
+        {
+            float xRot = Mathf.Clamp(
+                maxTiltDegree * d.y,
+                -maxTiltDegree,
+                maxTiltDegree
+            );
+            float zRot = Mathf.Clamp(
+                maxTiltDegree * d.x,
+                -maxTiltDegree,
+                maxTiltDegree
+            );
+            transform.localRotation = Quaternion.Euler(xRot, 0, -zRot);
+        }
     }
 }
